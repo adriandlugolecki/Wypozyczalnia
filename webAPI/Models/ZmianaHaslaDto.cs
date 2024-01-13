e2db7c5436cf7b6ff8106cee73c8f2b0e4e17f44
adriandlugolecki <73463589+adriandlugolecki@users.noreply.github.com>
2024-01-13 16:53:22 +0100

    recreate dist

--- webAPI/Models/ZmianaHaslaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace webAPI.Models
-{
-    public class ZmianaHaslaDto
-    {
-        public required string Email { get; set; }
-        public string Kod { get; set; } = string.Empty;
-        public string Haslo { get; set; } = string.Empty;
-    }
-}
